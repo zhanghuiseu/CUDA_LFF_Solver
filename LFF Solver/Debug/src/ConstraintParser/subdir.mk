a9d18073003bdde6c70cdfdeacc677200e4c8a84
Zhang Hui <2528558197@qq.com>
2017-11-18 21:20:43 +0800

    主要添加ATGC工具的初始部分

--- LFF Solver/Debug/src/ConstraintParser/subdir.mk
+++ LFF Solver/Debug/src/ConstraintParser/subdir.mk
@@ -3,22 +3,22 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CPP_SRCS += \
-../src/ConstraintParser/ConstraintParameter.cpp 
+CU_SRCS += \
+../src/ConstraintParser/ConstraintParameter.cu 
+
+CU_DEPS += \
+./src/ConstraintParser/ConstraintParameter.d 
 
 OBJS += \
 ./src/ConstraintParser/ConstraintParameter.o 
 
-CPP_DEPS += \
-./src/ConstraintParser/ConstraintParameter.d 
-
 
 # Each subdirectory must supply rules for building sources it contributes
-src/ConstraintParser/%.o: ../src/ConstraintParser/%.cpp
+src/ConstraintParser/%.o: ../src/ConstraintParser/%.cu
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
 	/usr/local/cuda-8.0/bin/nvcc -G -g -O0 -gencode arch=compute_50,code=sm_50  -odir "src/ConstraintParser" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-8.0/bin/nvcc -G -g -O0 --compile  -x c++ -o  "$@" "$<"
+	/usr/local/cuda-8.0/bin/nvcc -G -g -O0 --compile --relocatable-device-code=true -gencode arch=compute_50,code=compute_50 -gencode arch=compute_50,code=sm_50  -x cu -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
